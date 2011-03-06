342e11186100df255df36b4307597b4be2c67592
ifnubima <ifnubima@ddd900dc-19ee-11df-8006-ddd0014e6ec4>
2011-03-06 18:12:14 +0000

    connect

---
+++ src/main/java/com/tanyajava/controller/SignupController.java
@@ -0,0 +1,47 @@
+package com.tanyajava.controller;
+
+import com.tanyajava.model.User;
+import com.tanyajava.service.UserService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.social.web.signin.ProviderSignInUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class SignupController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public SignupForm signupForm() {
+        return new SignupForm();
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signup(@Valid SignupForm form, BindingResult result, WebRequest request) {
+        if (result.hasErrors()) {
+            return null;
+        }
+        boolean accountCreated = createAccount(form, result);
+        if (accountCreated) {
+            ProviderSignInUtils.handleConnectPostSignUp(form.getUsername(), request);
+            return "redirect:/";
+        }
+        return null;
+    }
+
+    // internal helpers
+    private boolean createAccount(SignupForm form, BindingResult formBinding) {
+        User user = new User();
+        userService.save(user);
+
+        return true;
+
+    }
+}
