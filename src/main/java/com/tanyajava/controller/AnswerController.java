9b05f451df09ba05e6ba5e10b3409d7b9843a9b0
ifnubima <ifnubima@ddd900dc-19ee-11df-8006-ddd0014e6ec4>
2011-03-08 01:52:41 +0000

    ask, answer, signin dan signup

--- src/main/java/com/tanyajava/controller/AnswerController.java
+++ src/main/java/com/tanyajava/controller/AnswerController.java
@@ -5,10 +5,32 @@
 
 package com.tanyajava.controller;
 
+import com.tanyajava.model.Question;
+import com.tanyajava.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author ifnu
  */
+@Controller
 public class AnswerController {
+
+    @Autowired private QuestionService questionService;
+
+    @RequestMapping(value="/answer/{id}",method=RequestMethod.GET)
+    public String answer(@PathVariable Long id, Model model){
+        Question q = questionService.getQuestion(id);
+        model.addAttribute(q);
+        //check if user already login or not
+        Boolean login = System.currentTimeMillis() % 2 == 0 ? Boolean.TRUE : Boolean.FALSE;
+        model.addAttribute("login", login);
+        return "/answer";
+    }
 
 }
--- src/main/java/com/tanyajava/model/Tag.java
+++ src/main/java/com/tanyajava/model/Tag.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import javax.persistence.UniqueConstraint;
 
 /**
  *
