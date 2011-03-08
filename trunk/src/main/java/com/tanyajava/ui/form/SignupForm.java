68ffd4212a7936161c1351d2b74a28ca51a87dc3
ifnubima <ifnubima@ddd900dc-19ee-11df-8006-ddd0014e6ec4>
2011-03-08 01:54:45 +0000

    ask, answer, signin dan signup

---
+++ trunk/src/main/java/com/tanyajava/ui/form/SignupForm.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tanyajava.ui.form;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class SignUpForm {
+
+	@NotEmpty
+	private String username;
+
+        @NotEmpty
+	@Size(min = 6, message = "must be at least 6 characters")
+	private String email;
+
+        @NotEmpty
+        @Size(min = 6, message = "must be at least 6 characters")
+	private String homePage;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public void setHomePage(String homePage) {
+        this.homePage = homePage;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
